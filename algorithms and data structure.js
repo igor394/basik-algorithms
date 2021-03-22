c946acf8b0b5297b1cd4fea6d3dbbb7e96eb3379
igor349 <igorsvidunovic@gmail.com>
2021-03-22 13:46:09 +0200

    note linear search

--- algorithms and data structure.js
+++ algorithms and data structure.js
@@ -1,1 +1,13 @@
-console.log('work')
+
+const array = [2,3,6,9,8,1,5,10,11,7,4,12];
+let counter = 0;
+
+// O(n) line search
+const lineAlgorithm = (array, elem)=>{
+    for(let i=0;i<array.length;i++){
+        counter++;
+        if(array[i]===elem) return i;
+    }
+    return null;
+}
+console.log(lineAlgorithm(array, 8),  counter);
